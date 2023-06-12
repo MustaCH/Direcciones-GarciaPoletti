ef51ceb3a052e6a302d6ee94a81a290e88d99702
Musta <polettiignacio7@gmail.com>
2023-06-12 07:04:13 -0300

    finalizado agregar features

---
+++ src/utils/colors.js
@@ -0,0 +1,10 @@
+export default {
+  primary: "#E1646B",
+  secondary: "#D46771",
+  tertiary: "#BC6C7E",
+  background: "#977490",
+  text: "#212121",
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: "#808080",
+};
